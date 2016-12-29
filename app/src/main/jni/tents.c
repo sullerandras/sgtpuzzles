bb41d4026ddaca6136b8e76bced4d3c9f53076bd
Andras Suller <suller.andras@gmail.com>
2016-12-29 22:53:30 +0800

    Fix clipped numbers in tents

--- app/src/main/jni/tents.c
+++ app/src/main/jni/tents.c
@@ -2520,7 +2520,7 @@ static void int_redraw(drawing *dr, game_drawstate *ds,
 	    draw_rect(dr, COORD(x), COORD(h)+1, TILESIZE, BRBORDER-1,
 		      COL_BACKGROUND);
 	    sprintf(buf, "%d", state->numbers->numbers[x]);
-	    draw_text(dr, COORD(x) + TILESIZE/2, COORD(h+1),
+	    draw_text(dr, COORD(x) + TILESIZE/2, COORD(h) + TILESIZE/2,
 		      FONT_VARIABLE, TILESIZE/2, ALIGN_HCENTRE|ALIGN_VNORMAL,
 		      (errors[w*h+x] ? COL_ERROR : COL_GRID), buf);
 	    draw_update(dr, COORD(x), COORD(h)+1, TILESIZE, BRBORDER-1);
@@ -2534,7 +2534,7 @@ static void int_redraw(drawing *dr, game_drawstate *ds,
 	    draw_rect(dr, COORD(w)+1, COORD(y), BRBORDER-1, TILESIZE,
 		      COL_BACKGROUND);
 	    sprintf(buf, "%d", state->numbers->numbers[w+y]);
-	    draw_text(dr, COORD(w+1), COORD(y) + TILESIZE/2,
+	    draw_text(dr, COORD(w) + TILESIZE/2, COORD(y) + TILESIZE/2,
 		      FONT_VARIABLE, TILESIZE/2, ALIGN_HRIGHT|ALIGN_VCENTRE,
 		      (errors[w*h+w+y] ? COL_ERROR : COL_GRID), buf);
 	    draw_update(dr, COORD(w)+1, COORD(y), BRBORDER-1, TILESIZE);
